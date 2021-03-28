5a78260299fe78fce1fb322d9a23dddf67a2907c
albertocarot1 <acaroli1990@gmail.com>
2021-03-28 20:29:18 +0200

    extra fixes, initial proxy adding

--- erowid_scraping.py
+++ erowid_scraping.py
@@ -1,34 +1,55 @@
 import json
+import logging
 import os
 import random
 from time import sleep
-from typing import Dict
+from typing import Dict, Optional
 from urllib.parse import urlparse
-import logging
 
 import requests
 import requests_cache
 from bs4 import BeautifulSoup, Tag, Comment, NavigableString
-from newspaper import Article
 
 requests_cache.install_cache('erowid_cache')
 logging.basicConfig(level=logging.DEBUG)
+
 
 class MissingExperience(Exception):
     pass
 
 
+class ProxyCredentials:
+    server: str
+    username: str
+    password: str
+
+    def __init__(self, cred_file):
+        with open(cred_file) as open_json:
+            credentials = json.load(open_json)
+        self.username = credentials["username"]
+        self.password = credentials["password"]
+        self.server = "it212.nordvpn.com"
+
+    def get_proxy(self):
+        proxy_string = f"{self.username}:{self.password}@{self.server}"
+        return {
+            'http': proxy_string,
+            'https': proxy_string
+        }
+
+
 class ExperienceScraper:
     url: str
     soup: BeautifulSoup
-    newspaper: Article
     exp_id: str
     save_path: str = ''
+    title: str = ''
     substances_details: list = []
     story: list = []
     substances_simple: list = []
     metadata: dict = {}
     tags: list = []
+    proxy: dict = {}
 
     def __init__(self, url: str):
         self.url = url
@@ -62,7 +83,7 @@ class ExperienceScraper:
 
         save_row = False
         for row in story_tag.contents:
-            if isinstance(row,Comment):
+            if isinstance(row, Comment):
                 row_str = str(row).strip()
                 if row_str == 'Start Body':
                     save_row = True
@@ -80,17 +101,24 @@ class ExperienceScraper:
         """
         Extracts substances usage table
         """
-
-        drugs_chart = self.soup.find_all(class_="dosechart")[0].find_all('tr')
+        dosechart_tag = self.soup.find_all(class_="dosechart")
+        drugs_chart = dosechart_tag[0].find_all('tr') if dosechart_tag else []
         for row in drugs_chart:
-            amount = row.find_all(class_='dosechart-amount')[0].text.strip()
-            method = row.find_all(class_='dosechart-method')[0].text.strip()
+            amount_tag = row.find_all(class_='dosechart-amount')
+            amount = amount_tag[0].text.strip() if amount_tag else ''
+
+            method_tag = row.find_all(class_='dosechart-method')
+            method = method_tag[0].text.strip() if method_tag else ''
+
             substance_tag = row.find_all(class_='dosechart-substance')[0]
             substance_ids = substance_tag.find_all('a')
             substance_id = substance_ids[0].attrs['href'].strip() if substance_ids else ''
             substance_name = substance_tag.text.strip()
-            form = row.find_all(class_='dosechart-form')[0].text.strip()
-            use_time = list(row.children)[1].text.replace("DOSE:","").strip()  # time to be extracted from text
+
+            form_tag = row.find_all(class_='dosechart-form')
+            form = form_tag[0].text.strip() if form_tag else ''
+
+            use_time = list(row.children)[1].text.replace("DOSE:", "").strip()  # time to be extracted from text
             self.substances_details.append({'use_time': use_time,
                                             'amount': amount,
                                             'method': method,
@@ -100,36 +128,37 @@ class ExperienceScraper:
 
     @staticmethod
     def _dict_from_str(text_str: str):
+        # Given a string, extract text and id and return in a dict
+        text_str = text_str.strip()
         parts = text_str.split('(')
-        s = parts[0].strip()
+        name = parts[0].strip()
         identifier = parts[1].strip().strip(')')
-        return {'name': s, 'id': identifier}
+        return {'name': name, 'id': identifier}
 
     def extract_experience_metadata(self):
         """
         Extract other metadata, tags, and substances one-hot
         """
-
+        self.title = self.soup.find(class_='title').text.strip()
         footdata_tag = self.soup.find(class_='footdata')
-        self.metadata = {'body_weight': self.soup.find(class_='bodyweight-amount').text.strip()}
+        bodyweight_tag = self.soup.find(class_='bodyweight-amount')
+        self.metadata = {'body_weight': bodyweight_tag.text.strip() if bodyweight_tag else ''}
 
         for row in footdata_tag:
-            if type(row) == Tag:
+            if isinstance(row, Tag):
                 if len(row.contents) > 1:
-                    text_list = row.contents[0].text.split(':')
-                    if len(text_list) == 2:
-                        self.metadata[text_list[0]] = text_list[1]
-                    text_list = row.contents[1].text.split(':')
-                    if len(text_list) == 2:
-                        self.metadata[text_list[0]] = text_list[1]
-                elif "View as PDF" not in row.text:
+                    for row_contents in row.contents:
+                        text_list = row_contents.text.split(':')
+                        if len(text_list) == 2:
+                            self.metadata[text_list[0]] = text_list[1]
+                elif "View as PDF" not in row.text.strip():
                     row_elements = row.text.split(':')
                     substances = row_elements[0].split(',')
                     for sub in substances:
                         self.substances_simple.append(self._dict_from_str(sub))
                     tags_list = row_elements[1].split(',')
-                    for t in tags_list:
-                        self.tags.append(self._dict_from_str(t))
+                    for tag in tags_list:
+                        self.tags.append(self._dict_from_str(tag))
 
     def save_as_json(self):
         """
@@ -145,14 +174,18 @@ class ExperienceScraper:
                 'substances_main': self.substances_simple,
                 'metadata': self.metadata,
                 'tags': self.tags,
-                'title': self.newspaper.title}
+                'title': self.title}
 
 
 class ErowidScraper:
+    raise_exceptions: bool
     seed: int = 666
     save_folder: str = "experiences_db"
     experiences_to_download: Dict[str, ExperienceScraper] = {}
     base_url: str = "https://www.erowid.org/experiences/exp.php?ID="
+
+    def __init__(self, raise_exceptions: bool = False, proxy_server: Optional[dict] = None):
+        self.raise_exceptions = raise_exceptions
 
     def download(self, wait: bool = False):
         """
@@ -163,23 +196,24 @@ class ErowidScraper:
         urls_failed = 0
 
         for i, exp_scraper in enumerate(self.experiences_to_download.values()):
-            if wait:
-                sleep(random.randint(3, 20))
             try:
                 logging.info(f"Downloading {exp_scraper.url}...")
                 exp_scraper.get_experience()
                 exp_scraper.extract_data()
-
+                exp_scraper.save_as_json()
                 urls_downloaded += 1
                 logging.info(f"success. So far {urls_downloaded} pages downloaded correctly.")
             except Exception as e:
+                if self.raise_exceptions:
+                    raise
                 logging.exception('failed:')
                 with open(f'exp_links/failed_urls_{type(e).__name__}.txt', 'a') as open_txt:
                     open_txt.write(exp_scraper.url)
                     open_txt.write('\n')
                 urls_failed += 1
                 logging.error(f"So far {urls_failed} errors.")
-
+            if wait:
+                sleep(random.randint(3, 20))
 
     def update_download_list(self, file: str = ''):
         """
@@ -207,17 +241,18 @@ class ErowidScraper:
             exp_scraper = ExperienceScraper(url)
             candidate_path = os.path.join(self.save_folder, f"{exp_scraper.exp_id}.json")
             if os.path.isfile(candidate_path):
-                print(f"Experience {exp_scraper.exp_id} already downloaded")
+                logging.info(f"Experience {exp_scraper.exp_id} already downloaded")
             else:
                 exp_scraper.save_path = candidate_path
                 self.experiences_to_download[exp_scraper.exp_id] = exp_scraper
 
 
 def main():
-    erowid_scraper = ErowidScraper()
+    proxy = ProxyCredentials("credentials.json")
+    erowid_scraper = ErowidScraper(raise_exceptions=True, proxy_server=proxy.get_proxy())
     erowid_scraper.update_download_list('exp_links/mystical_experiences.txt')
     erowid_scraper.update_download_list('exp_links/bad_trips.txt')
-    erowid_scraper.download()
+    erowid_scraper.download(wait=True)
 
 
 if __name__ == '__main__':
