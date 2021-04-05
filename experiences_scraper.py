39448734b1200b4251931a40b40d33ba100f5c87
albertocarot1 <acaroli1990@gmail.com>
2021-04-06 00:43:36 +0200

    refactored scraping code

--- experiences_scraper.py
+++ experiences_scraper.py
@@ -2,15 +2,14 @@ import json
 import logging
 import os
 import random
-from time import sleep
-from typing import Dict, Optional, List
+from typing import Optional
 from urllib.parse import urlparse
 
 import requests
 import requests_cache
-from bs4 import BeautifulSoup, Tag, Comment, NavigableString
+from bs4 import Tag, Comment, NavigableString
 
-from utils import from_txt_to_list
+from utils import from_txt_to_list, ProxyServer, ElementScraper, ListScraper
 
 requests_cache.install_cache('erowid_cache')
 logging.basicConfig(level=logging.INFO)
@@ -24,92 +23,29 @@ class ThrottledException(Exception):
     pass
 
 
-class ProxyServer:
-    servers: List[str]
-    username: str
-    password: str
-    server_number: int
-    server_in_use: str
-
-    def __init__(self, cred_file):
-        """
-        Use credentials file to setup proxy.
-        Initialise servers in a random order,
-        :param cred_file:
-        """
-        with open(cred_file) as open_json:
-            credentials = json.load(open_json)
-        self.username = credentials["username"]
-        self.password = credentials["password"]
-        self.servers = credentials["servers"]
-        assert len(self.servers) > 0
-        random.shuffle(self.servers)
-        self.server_number = 0
-        self.server_in_use = self.servers[self.server_number]
-
-    def update_server_used(self):
-        """
-        Change the server in use with the next one in `servers` list
-        """
-        if self.server_number < len(self.servers):
-            self.server_number += 1
-        else:
-            self.server_number = 0
-        self.server_in_use = self.servers[self.server_number]
-
-    def get_proxy(self):
-        """
-        Return proxy credentials to use to make socks calls though requests
-        :return: dict with credenials and server for http and https
-        """
-        proxy_string = f"socks5://{self.username}:{self.password}@{self.server_in_use}:1080"
-        return {
-            'http': proxy_string,
-            'https': proxy_string
-        }
-
-
-
-class ExperienceScraper:
-    url: str
-    soup: BeautifulSoup
+class ExperienceScraper(ElementScraper):
     exp_id: str
-    save_path: str = ''
     title: str = ''
     substances_details: list = []
     story: list = []
     substances_simple: list = []
     metadata: dict = {}
     tags: list = []
-    proxy_server: Optional[ProxyServer] = None
-    was_cached: bool = False
-    headers: dict = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"}
 
     def __init__(self, url: str, proxy_server: Optional[ProxyServer] = None):
-        self.url = url
+        super().__init__(url, proxy_server)
         self.exp_id = urlparse(url).query.split('=')[1]
-        self.proxy_server = proxy_server
 
-    def get_experience(self):
+    def http_call(self, proxy):
         """
-        Retrieve the experience HTML code and input it
-        for further processing
+        Make the call to the experiences url
+        :param proxy: proxy server to use for the call
+        :return:
         """
-        proxy = self.proxy_server.get_proxy() if self.proxy_server else None
-        res = requests.get(self.url,
-                           proxies=proxy,
-                           headers=self.headers,
-                           allow_redirects=False)
-        res.raise_for_status()
-        if res.text.find("IP address has been blocked") != -1 and proxy is not None:
-            self.proxy_server.update_server_used()
-            requests.Session().cache.delete_url(self.url)
-            res = requests.get(self.url,
-                               proxies=self.proxy_server.get_proxy(),
-                               headers=self.headers,
-                               allow_redirects=False)
-        self.was_cached = res.from_cache
-        self.soup = BeautifulSoup(res.content, 'html.parser')
+        return requests.get(self.url,
+                            proxies=proxy,
+                            headers=self.headers,
+                            allow_redirects=False)
 
     def extract_data(self):
         """
@@ -207,7 +143,7 @@ class ExperienceScraper:
                     for tag in tags_list:
                         self.tags.append(self._dict_from_str(tag))
 
-    def save_as_json(self):
+    def save(self):
         """
         Save the experience as a JSON file in the `save_path`
         """
@@ -224,54 +160,13 @@ class ExperienceScraper:
                 'title': self.title}
 
 
-class ErowidScraper:
-    raise_exceptions: bool
-    seed: int = 666
+class ErowidScraper(ListScraper):
     save_folder: str = "experiences_db"
-    experiences_to_download: Dict[str, ExperienceScraper] = {}
     base_url: str = "https://www.erowid.org/experiences/exp.php?ID="
-    min_wait: int = 10
-    max_wait: int = 30
-    proxy_server: Optional[Dict[str, str]]
-
-    def __init__(self, raise_exceptions: bool = False, proxy_server: Optional[ProxyServer] = None):
-        self.raise_exceptions = raise_exceptions
-        self.proxy_server = proxy_server
-
-    def download(self, wait: bool = False):
-        """
-        Download all the urls contained in urls_to_be_downloaded
-        Wait for a random interval between a range, if needed.
-        Never wait when the url to download was already cached.
-
-        :param wait: Whether it should wait a random number of seconds (in a range) between downloads
-        """
-        urls_downloaded = 0
-        urls_failed = 0
-        logging.info(f"A total of {len(self.experiences_to_download)} links will be attempted to download")
-        for i, exp_scraper in enumerate(self.experiences_to_download.values()):
-            try:
-                logging.info(f"Downloading {exp_scraper.url}...")
-                exp_scraper.get_experience()
-                exp_scraper.extract_data()
-                exp_scraper.save_as_json()
-                urls_downloaded += 1
-                logging.info(f"success. So far {urls_downloaded} pages downloaded correctly.")
-            except Exception as e:
-                if self.raise_exceptions:
-                    raise
-                logging.exception('failed:')
-                with open(f'exp_links/failed_urls_{type(e).__name__}.txt', 'a') as open_txt:
-                    open_txt.write(exp_scraper.url)
-                    open_txt.write('\n')
-                urls_failed += 1
-                logging.error(f"So far {urls_failed} errors.")
-            if wait and not exp_scraper.was_cached:
-                sleep(random.randint(self.min_wait, self.max_wait))
 
     def update_download_list(self, file: str = ''):
         """
-        Create a list of URLs to download.
+        Update the list of URLs to download.
         This list can come directly from a txt file, or can be created
         with ids that might be real or not (as ids are sparse in the
         selected range).
@@ -294,17 +189,13 @@ class ErowidScraper:
                 logging.debug(f"Experience {exp_scraper.exp_id} already downloaded")
             else:
                 exp_scraper.save_path = candidate_path
-                self.experiences_to_download[exp_scraper.exp_id] = exp_scraper
+                self.urls_to_download[exp_scraper.exp_id] = exp_scraper
 
 
 def main():
     proxy = ProxyServer("credentials.json")
-    erowid_scraper = ErowidScraper(raise_exceptions=True, proxy_server=proxy)
-    erowid_scraper.update_download_list('exp_links/failed_urls_ConnectionError.txt')
-    erowid_scraper.update_download_list('exp_links/failed_urls_HTTPError.txt')
-    erowid_scraper.update_download_list('exp_links/failed_urls_IndexError.txt')
-    erowid_scraper.update_download_list('exp_links/failed_urls_MissingExperience.txt')
-    # erowid_scraper.update_download_list()
+    erowid_scraper = ErowidScraper(raise_exceptions=False, proxy_server=proxy)
+    erowid_scraper.update_download_list()
     erowid_scraper.download(wait=True)
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
-from typing import List
+import json
+import logging
+import random
+from time import sleep
+from typing import List, Optional, Dict
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def from_txt_to_list(txt_path: str) -> List[str]:
@@ -13,3 +20,134 @@ def from_txt_to_list(txt_path: str) -> List[str]:
             if line.strip():
                 rows.append(line.strip())
     return rows
+
+
+class ProxyServer:
+    servers: List[str]
+    username: str
+    password: str
+    server_number: int
+    server_in_use: str
+
+    def __init__(self, cred_file):
+        """
+        Use credentials file to setup proxy.
+        Initialise servers in a random order,
+        :param cred_file:
+        """
+        with open(cred_file) as open_json:
+            credentials = json.load(open_json)
+        self.username = credentials["username"]
+        self.password = credentials["password"]
+        self.servers = credentials["servers"]
+        assert len(self.servers) > 0
+        random.shuffle(self.servers)
+        self.server_number = 0
+        self.server_in_use = self.servers[self.server_number]
+
+    def update_server_used(self):
+        """
+        Change the server in use with the next one in `servers` list
+        """
+        if self.server_number < len(self.servers):
+            self.server_number += 1
+        else:
+            self.server_number = 0
+        self.server_in_use = self.servers[self.server_number]
+
+    def get_proxy(self):
+        """
+        Return proxy credentials to use to make socks calls though requests
+        :return: dict with credenials and server for http and https
+        """
+        proxy_string = f"socks5://{self.username}:{self.password}@{self.server_in_use}:1080"
+        return {
+            'http': proxy_string,
+            'https': proxy_string
+        }
+
+
+class ElementScraper:
+    url: str
+    soup: BeautifulSoup
+    save_path: str = ''
+    proxy_server: Optional[ProxyServer] = None
+    was_cached: bool = False
+    headers: dict = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"}
+
+    def __init__(self, url: str, proxy_server: Optional[ProxyServer] = None):
+        self.url = url
+        self.proxy_server = proxy_server
+
+    def get(self):
+        """
+        Retrieve the experience HTML code and input it
+        for further processing
+        """
+        proxy = self.proxy_server.get_proxy() if self.proxy_server else None
+        res = self.http_call(proxy)
+        res.raise_for_status()
+        if res.text.find("IP address has been blocked") != -1 and proxy is not None:
+            self.proxy_server.update_server_used()
+            requests.Session().cache.delete_url(self.url)
+            res = self.http_call(self.proxy_server.get_proxy())
+        self.was_cached = res.from_cache
+        self.soup = BeautifulSoup(res.content, 'html.parser')
+
+    def http_call(self, proxy):
+        raise NotImplementedError("method http_call must be implemented")
+
+    def extract_data(self):
+        raise NotImplementedError("method extract_data must be implemented")
+
+    def save(self):
+        raise NotImplementedError("method save must be implemented")
+
+
+class ListScraper:
+    raise_exceptions: bool
+    seed: int = 666
+    save_folder: str = ""
+    urls_to_download: Dict[str, ElementScraper] = {}
+    base_url: str = ""
+    min_wait: int = 10
+    max_wait: int = 30
+    proxy_server: Optional[Dict[str, str]]
+
+    def __init__(self, raise_exceptions: bool = False, proxy_server: Optional[ProxyServer] = None):
+        self.raise_exceptions = raise_exceptions
+        self.proxy_server = proxy_server
+
+    def download(self, wait: bool = False):
+        """
+        Download all the urls contained in urls_to_be_downloaded
+        Wait for a random interval between a range, if needed.
+        Never wait when the url to download was already cached.
+
+        :param wait: Whether it should wait a random number of seconds (in a range) between downloads
+        """
+        urls_downloaded = 0
+        urls_failed = 0
+        logging.info(f"A total of {len(self.urls_to_download)} links will be attempted to download")
+        for i, exp_scraper in enumerate(self.urls_to_download.values()):
+            try:
+                logging.info(f"Downloading {exp_scraper.url}...")
+                exp_scraper.get()
+                exp_scraper.extract_data()
+                exp_scraper.save()
+                urls_downloaded += 1
+                logging.info(f"success. So far {urls_downloaded} pages downloaded correctly.")
+            except Exception as e:
+                if self.raise_exceptions:
+                    raise
+                logging.exception('failed:')
+                with open(f'exp_links/failed_urls_{type(e).__name__}.txt', 'a') as open_txt:
+                    open_txt.write(exp_scraper.url)
+                    open_txt.write('\n')
+                urls_failed += 1
+                logging.error(f"So far {urls_failed} errors.")
+            if wait and not exp_scraper.was_cached:
+                sleep(random.randint(self.min_wait, self.max_wait))
+
+    def update_download_list(self):
+        raise NotImplementedError
